ff7c314266b7168cfa16d502260c96adf23d84b5
trachemys-scripta57 <6416417@gmail.com>
2022-09-22 12:49:52 +0300

    Тестируем области видимости (scope) объектов (beans) - singleton (по-умолчанию, helloworld) и созданный нами prototype (cat). Каждый новый объект helloworld это ссылка на один единственный, ранеесозданный. Тогда как, при создании объектов cat - каждый раз создаётся новый.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,13 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        System.out.println("Равны ли бины HelloWorld между собой? - " + (bean == bean2));
+
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println("Равны ли бины Cat между собой? - " + (catBean == catBean2));
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,16 @@
+public class Cat {
+    private String cat_id;
+
+    public Cat() {
+
+    }
+
+    public String getCat_id() {
+        return cat_id;
+    }
+
+    public void setCat_id(String cat_id) {
+        this.cat_id = cat_id;
+    }
+}
+// типа Model..
